05f820412d6394e56ca96575cd54d033616e64ef
Wai Yan Win Htain <48019325+waiyanwh@users.noreply.github.com>
2022-11-14 15:00:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// test to trigger ci
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
